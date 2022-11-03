12604ca87716beb0a6285d547b19ef5853118aac
Jose Antonio Ponqueli Conto <zehconto@gmail.com>
2022-11-02 21:58:51 -0300

    study why to use styled component

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,11 @@ import { Button } from "./components/Button";
 export function App() {
   return (
     <>
-      <Button />
+      <Button color="primary" />
+      <Button color="secondary" />
+      <Button color="success" />
+      <Button color="danger" />
+
       <Button />
     </>
   );
--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,3 +1,13 @@
-export function Button() {
-  return <button type="button">Enviar</button>;
+import styles from "./Button.module.css";
+
+interface IButtonProps {
+  color?: "primary" | "secondary" | "danger" | "success";
+}
+
+export function Button({ color = "primary" }: IButtonProps) {
+  return (
+    <button type="button" className={`${styles.button} ${styles[color]}`}>
+      Enviar
+    </button>
+  );
 }
