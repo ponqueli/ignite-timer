90072c655d937800077b3a8b26a05a58a28f12f2
Jose Antonio Ponqueli Conto <zehconto@gmail.com>
2022-11-05 11:16:40 -0300

    wip: add headercontainer

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,12 @@
+import { HeaderContainer } from "./styles";
+
+export function Header() {
+  return (
+    <HeaderContainer>
+      <nav>
+        <a href="/"> Home </a>
+        <a href="/history"> History </a>
+      </nav>
+    </HeaderContainer>
+  );
+}
--- src/layouts/DefaultLayout/index.tsx
+++ src/layouts/DefaultLayout/index.tsx
@@ -1,12 +1,13 @@
 import { Outlet } from "react-router-dom";
-import { Header } from "../components/Header";
+import { Header } from "../../components/Header";
+import { LayoutContainer } from "./styles";
 
 export function DefaultLayout() {
   return (
-    <>
+    <LayoutContainer>
       <Header />
       {/* OUTLET = espaço para ser inserido um conteudo */}
       <Outlet />
-    </>
+    </LayoutContainer>
   );
 }
