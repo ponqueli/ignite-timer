7cf5d0740bc94c6a81288ddbafd8aaa303a88d34
Jose Antonio Ponqueli Conto <zehconto@gmail.com>
2022-11-07 18:02:05 -0300

    feat: add main content and started styling

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,31 @@
+import { Play } from "phosphor-react";
+import { HomeContainer, FormContainer, CountdownContainer } from "./styles";
+
+export function Home() {
+  return (
+    <HomeContainer>
+      <form action="">
+        <FormContainer>
+          <label htmlFor="task">Vou trabalhar em</label>
+          <input id="task" />
+          <label htmlFor="minutesAmount">durante</label>
+          <input id="minutesAmount" type="number" />
+          <span>minutos.</span>
+        </FormContainer>
+
+        <CountdownContainer>
+          <span>0</span>
+          <span>0</span>
+          <span>:</span>
+          <span>0</span>
+          <span>0</span>
+        </CountdownContainer>
+
+        <button type="submit">
+          <Play size={24} />
+          Começar
+        </button>
+      </form>
+    </HomeContainer>
+  );
+}
