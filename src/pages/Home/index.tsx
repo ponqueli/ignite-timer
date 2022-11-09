0a5639750b6a51dc25614272d5dca9f814fb6750
Jose Antonio Ponqueli Conto <zehconto@gmail.com>
2022-11-08 22:43:22 -0300

    feat: finish styling home page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -14,23 +14,26 @@ export function Home() {
     <HomeContainer>
       <form action="">
         <FormContainer>
-          <label htmlFor="task">
-            Vou trabalhar em
-            <TaskInput id="task" placeholder="Dê um nome para o seu projeto" />
-          </label>
-
-          <label htmlFor="minutesAmount">
-            durante
-            <MinutesAmountInput
-              id="minutesAmount"
-              type="number"
-              placeholder="00"
-              step={5}
-              min={5}
-              max={60}
-            />
-          </label>
-
+          <label htmlFor="task">Vou trabalhar em</label>
+          <TaskInput
+            id="task"
+            placeholder="Dê um nome para o seu projeto"
+            list="taskSuggestions"
+          />
+          <datalist id="taskSuggestions">
+            <option value="Projeto 1" />
+            <option value="Projeto 2" />
+            <option value="Projeto Banana" />
+          </datalist>
+          <label htmlFor="minutesAmount">durante</label>
+          <MinutesAmountInput
+            id="minutesAmount"
+            type="number"
+            placeholder="00"
+            step={5}
+            min={5}
+            max={60}
+          />
           <span>minutos.</span>
         </FormContainer>
 
